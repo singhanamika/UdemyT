392110bd6a4903f029e40e998cb2c0c715441bb2
singhanamika <anasingh1593@gmail.com>
2018-03-22 00:30:33 -0400

    Update reusable.component.ts

--- src/app/reusable/reusable.component.ts
+++ src/app/reusable/reusable.component.ts
@@ -9,11 +9,27 @@ export class ReusableComponent implements OnInit {
 	
 review = { 
 howMany:15
-Menu: ["home", "News", "Contact", "about"]
+
+
+}
+review1 = { 
+how:12,
+Men: ["hme", "News", "Contt", "abut"]
 
 }
 
-	
+course1 = {course:	[
+  { id: 11, name: 'Mr. Nice', age: 15, subject: 'Science' },
+  { id: 12, name: 'Narco', age: 15, subject: 'Science'  },
+  { id: 13, name: 'Bombasto', age: 15, subject: 'Science'  },
+  { id: 14, name: 'Celeritas' , age: 15, subject: 'Science' },
+  { id: 15, name: 'Magneta', age: 15, subject: 'Science'  },
+  { id: 16, name: 'RubberMan', age: 15, subject: 'Science'  },
+  { id: 17, name: 'Dynama', age: 15, subject: 'Science'  },
+  { id: 18, name: 'Dr IQ', age: 15, subject: 'Science'  },
+  { id: 19, name: 'Magma', age: 15, subject: 'Science'  },
+  { id: 20, name: 'Tornado', age: 15, subject: 'Science'  }
+]};
 	
 
   constructor() { }
